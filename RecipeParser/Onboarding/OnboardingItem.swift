66b65d4c5729f72982a0089078fa32d67b519e86
Jason Jon Carreos <jasondev06@gmail.com>
2025-04-28 16:55:33 +1000

    Onboarding buttons and layout

---
+++ RecipeParser/Onboarding/OnboardingItem.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingItem.swift
+//  RecipeParser
+//
+//  Created by Jason Jon Carreos on 28/4/2025.
+//
+
+struct OnboardingItem: Hashable {
+    var title: String
+    var caption: String
+    var icon: String
+}
