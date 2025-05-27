5f483c5f1302135987a295e37950e5a8406117a6
Jason Jon Carreos <jasondev06@gmail.com>
2025-05-27 11:04:00 +1000

    Restructed to Screens

---
+++ RecipeParser/Screens/Recipe/RecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeView.swift
+//  RecipeParser
+//
+//  Created by Jason Jon Carreos on 26/5/2025.
+//
+
+import SwiftUI
+
+struct RecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeView()
+}
