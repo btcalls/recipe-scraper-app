2633acafde0f32c2b07f453b0293f7a3b86282e4
Jason Jon Carreos <jasondev06@gmail.com>
2025-06-28 21:49:31 +1000

    BottomControlView

---
+++ RecipeParser/Views/BottomControlView.swift
@@ -0,0 +1,37 @@
+//
+//  BottomControlView.swift
+//  RecipeParser
+//
+//  Created by Jason Jon Carreos on 28/6/2025.
+//
+
+import SwiftUI
+
+struct BottomControlView<Content>: View where Content : View {
+    var isVertical = false
+
+    @ViewBuilder let content: Content
+    
+    @ScaledMetric private var spacing: CGFloat = 10
+    
+    private var layout: AnyLayout {
+        return isVertical ?
+            AnyLayout(VStackLayout(alignment: .center, spacing: spacing)) :
+            AnyLayout(HStackLayout(alignment: .center, spacing: spacing))
+    }
+    
+    var body: some View {
+        layout {
+            content
+        }
+        .scale(.padding(.all), 10)
+        .background(Color.appBackground)
+        .clipShape(.capsule)
+        .shadow()
+    }
+}
+#Preview {
+    BottomControlView {
+        Text("Hello, world!")
+    }
+}
